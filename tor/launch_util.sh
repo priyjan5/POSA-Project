558b1bb78c068a8ee149070a6bdace16316da2f1
Joseph Dane <Joseph_Dane@symantec.com>
2016-10-31 11:50:10 -0400

    karbarbarkar

--- tor/launch_util.sh
+++ tor/launch_util.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+echo "[!] Updating package manager"
+sudo apt-get -y update
+
+
+echo "[!] installing openssh server"
+sudo apt-get install -y openssh-server > /dev/null
+
+
 useradd tor
 passwd tor
 usermod -aG sudo tor
