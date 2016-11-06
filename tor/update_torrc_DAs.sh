f9145ec8c09bef77235313b0c555159db2ec3e01
98Giraffe <jwd6500@rit.edu>
2016-11-06 14:08:30 -0500

    Update update_torrc_DAs.sh

--- tor/update_torrc_DAs.sh
+++ tor/update_torrc_DAs.sh
@@ -2,11 +2,16 @@ UTIL_SERVER=172.16.106.158
 sshpass -p "wordpass" scp -oStrictHostKeyChecking=no tor@${UTIL_SERVER}:~/DAs .
 
 while read p; do
-  
+  	FLAG=0
 	cat /etc/tor/torrc | grep "${p}"
-	if [ $? == 1 ]; then
+	if [ $? != 0 ]; then
 		echo "Adding line $p"
 		echo $p >> /etc/tor/torrc
+		$FLAG=1
 	fi
-
 done <./DAs
+
+if [ $FLAG ]; then
+	service tor restart
+fi
+
