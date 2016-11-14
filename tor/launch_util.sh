49016d0105eb14a7375ed17d02c7b7bccaeb3623
98Giraffe <jwd6500@rit.edu>
2016-11-14 00:14:24 -0500

    Update launch_util.sh

--- tor/launch_util.sh
+++ tor/launch_util.sh
@@ -7,6 +7,18 @@ sudo apt-get -y update
 echo "[!] installing openssh server"
 sudo apt-get install -y openssh-server > /dev/null
 
+echo "[!] installing apache2"
+sudo apt-get install -y apache2 > /dev/null
+
+echo "[!] installing git"
+sudo apt-get install -y git > /dev/null
+
+echo "[!] cloning Capstone Repo"
+git clone https://github.com/98Giraffe/RIT_Capstone_2016.git
+
+echo "[!] Copying deploy.sh to apache root"
+cp RIT_Capstone_2016/tor/deploy.sh /var/www/html/
+cp RIT_Capstone_2016/tor/update_torrc_DAs.sh /var/www/html/
 
 useradd tor
 passwd tor
