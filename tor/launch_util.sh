19c8c88e7dffa42eab173cb34f0c455ae557949a
98Giraffe <jwd6500@rit.edu>
2016-11-14 00:21:24 -0500

    Update launch_util.sh

--- tor/launch_util.sh
+++ tor/launch_util.sh
@@ -21,7 +21,7 @@ cp RIT_Capstone_2016/tor/deploy.sh /var/www/html/
 cp RIT_Capstone_2016/tor/update_torrc_DAs.sh /var/www/html/
 
 useradd tor
-passwd tor
+echo wordpass | passwd tor
 usermod -aG sudo tor
 
 cd /home/tor
