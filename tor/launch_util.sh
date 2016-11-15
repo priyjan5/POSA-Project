ecdd4544fb7d9c77cc94e49caf4240e01f0abc8a
98Giraffe <jwd6500@rit.edu>
2016-11-15 13:13:17 -0500

    Update launch_util.sh

--- tor/launch_util.sh
+++ tor/launch_util.sh
@@ -18,14 +18,21 @@ git clone https://github.com/98Giraffe/RIT_Capstone_2016.git
 
 echo "[!] Copying deploy.sh to apache root"
 cp RIT_Capstone_2016/tor/deploy.sh /var/www/html/
+
+echo "[!] Copying update_torrc_DAs.sh to apache root"
 cp RIT_Capstone_2016/tor/update_torrc_DAs.sh /var/www/html/
 
+echo "[!] Adding User tor and setting password to wordpass
 useradd -m -d /home/tor tor
 echo tor:wordpass | chpasswd tor
 usermod -aG sudo tor
 
+echo "[!] Creating DAs file in /home/tor and correcting file permissions
 cd /home/tor
 touch DAs
 chown tor DAs
 chgrp tor DAs
 chmod a+rw DAs
+
+echo "[!] Utility Server has been deployed"
+echo "... end."
